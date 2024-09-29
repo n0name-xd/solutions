f96ea2a04b445916d0fcc474ea4556492c7f7bb2
Evgeny_Grinchuk <n0name-xd@inbox.ru>
2024-09-29 20:07:53 +0300

    feat finished TrafficLIghtIndicator

--- src/components/ImgAppearance.tsx
+++ src/components/ImgAppearance.tsx
@@ -1,44 +1,30 @@
-import { RefObject, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import img from "../assets/react.svg";
 import img2 from "../assets/images.png";
 
-export const TrafficLIghtIndicator = () => {
-  return <></>;
-};
-
-export const ImgAppearance = (): JSX.Element => {
+interface ITrafficLIghtIndicatorProps {
+  img: string;
+  indicatorColor: string;
+}
+export const TrafficLIghtIndicator = ({
+  img,
+  indicatorColor,
+}: ITrafficLIghtIndicatorProps) => {
   const [isShowFormula, setIsShowFormula] = useState<boolean>(false);
   const [trigger, setTrigger] = useState<boolean>(false);
   const timerRef = useRef<NodeJS.Timeout | null>(null);
-  const marginalityRef = useRef<HTMLDivElement>(null);
-  const incomeRef = useRef<HTMLDivElement>(null);
-  const [imgItem, setImgItem] = useState<string>(img);
-  const [currentRef, setCurrentRef] = useState<RefObject<HTMLDivElement>>();
+  const indicatorRef = useRef<HTMLDivElement>(null);
 
-  const handleMouseEnter = (ref: RefObject<HTMLDivElement>) => {
-    setTrigger(true);
-    setCurrentRef(ref);
-  };
-
-  const handleMouseLeave = () => {
+  const handleMouseLeave = useCallback(() => {
     setTrigger(false);
     if (timerRef.current) {
       clearTimeout(timerRef.current);
       timerRef.current = null;
     }
     setIsShowFormula(false);
-  };
+  }, []);
 
-  const setFormulaPosition = (trigger: boolean) => {
-    if (currentRef) {
-      const elem = currentRef.current;
-      if (elem) {
-        const imgT = elem.getAttribute("data-formula");
-
-        setImgItem(imgT === "react.svg" ? img : img2);
-      }
-    }
-
+  const setFormulaPosition = useCallback((trigger: boolean) => {
     if (trigger) {
       timerRef.current = setTimeout(() => {
         if (trigger) {
@@ -46,7 +32,7 @@ export const ImgAppearance = (): JSX.Element => {
         }
       }, 900);
     }
-  };
+  }, []);
 
   useEffect(() => {
     setFormulaPosition(trigger);
@@ -56,37 +42,38 @@ export const ImgAppearance = (): JSX.Element => {
         clearTimeout(timerRef.current);
       }
     };
-  }, [trigger]);
+  }, [trigger, setFormulaPosition]);
 
+  return (
+    <div className="relative">
+      <div
+        ref={indicatorRef}
+        onMouseEnter={() => setTrigger(true)}
+        onMouseLeave={handleMouseLeave}
+        className={`w-4 h-4 rounded-full ${indicatorColor}`}
+      ></div>
+      {isShowFormula && (
+        <div className="border-[10px] border-black w-10 h-10 rounded-md absolute top-[-50px] left-[10px]">
+          <img src={img} alt="formula" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const ImgAppearance = (): JSX.Element => {
   return (
     <div className="w-full flex justify-center h-full items-center mt-10">
       <div className="relative border-2 border-red-700 w-96 h-96 rounded-2xl">
         <div className="flex gap-4 items-center  mt-20 ml-20">
-          <div
-            ref={marginalityRef}
-            data-formula="react.svg"
-            onMouseEnter={() => handleMouseEnter(marginalityRef)}
-            onMouseLeave={handleMouseLeave}
-            className="w-4 h-4 rounded-full bg-green-700"
-          ></div>
+          <TrafficLIghtIndicator img={img} indicatorColor="bg-green-700" />
+
           <div>marginality</div>
         </div>
         <div className="flex gap-4 items-center  mt-20 ml-20">
-          <div
-            ref={incomeRef}
-            data-formula="images.png"
-            onMouseEnter={() => handleMouseEnter(incomeRef)}
-            onMouseLeave={handleMouseLeave}
-            className="w-4 h-4 rounded-full bg-slate-500"
-          ></div>
+          <TrafficLIghtIndicator img={img2} indicatorColor="bg-red-600" />
           <div>income</div>
         </div>
-
-        {isShowFormula && (
-          <div className="border-[10px] border-black w-10 h-10 rounded-md absolute top-0 left-0">
-            <img src={imgItem} alt="formula" />
-          </div>
-        )}
       </div>
     </div>
   );
